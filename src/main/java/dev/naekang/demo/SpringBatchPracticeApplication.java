e828e3f7b91ebd9c385bcc2d6b78f2319a41d7f9
naekang <rlawlsgh6306@gmail.com>
2022-04-01 01:47:20 +0900

    간단하게 Spring Batch 시작해보기

--- src/main/java/dev/naekang/demo/SpringBatchPracticeApplication.java
+++ src/main/java/dev/naekang/demo/SpringBatchPracticeApplication.java
@@ -1,9 +1,11 @@
 package dev.naekang.demo;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchPracticeApplication {
 
 	public static void main(String[] args) {
