1d5ea83755e864af4f3a368f4df02fbaceb133ec
naekang <rlawlsgh6306@gmail.com>
2022-04-01 01:18:30 +0900

    Initial commit

---
+++ src/test/java/dev/naekang/demo/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.naekang.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
