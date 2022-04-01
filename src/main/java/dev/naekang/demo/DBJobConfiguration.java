e8487990a3deb9bc2773de59ccaafb3f44b7fd9b
naekang <rlawlsgh6306@gmail.com>
2022-04-01 18:00:59 +0900

    DB 스키마 생성

--- src/main/java/dev/naekang/demo/DBJobConfiguration.java
+++ src/main/java/dev/naekang/demo/DBJobConfiguration.java
@@ -14,30 +14,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class HelloJobConfiguration {
+public class DBJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job helloJob() {
-        return jobBuilderFactory.get("helloJob")
-                .start(helloStep1())
-                .next(helloStep2())
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
                 .build();
     }
 
     @Bean
-    public Step helloStep1() {
-        return stepBuilderFactory.get("helloStep1")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-
-                        System.out.println("=====================");
-                        System.out.println(">>Hello Spring Batch!");
-                        System.out.println("=====================");
-
+                        System.out.println("step1 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
@@ -45,16 +41,12 @@ public class HelloJobConfiguration {
     }
 
     @Bean
-    public Step helloStep2() {
-        return stepBuilderFactory.get("helloStep2")
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-
-                        System.out.println("=====================");
-                        System.out.println(">>step2 was started");
-                        System.out.println("=====================");
-
+                        System.out.println("step2 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
