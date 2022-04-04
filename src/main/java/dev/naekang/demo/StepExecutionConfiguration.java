a8e053d418318648de1ff53245d8566aa41e7caf
naekang <rlawlsgh6306@gmail.com>
2022-04-04 21:35:56 +0900

    StepExecution

--- src/main/java/dev/naekang/demo/StepExecutionConfiguration.java
+++ src/main/java/dev/naekang/demo/StepExecutionConfiguration.java
@@ -14,23 +14,30 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class StepConfiguration {
+public class StepExecutionConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job job() {
-        return jobBuilderFactory.get("job")
+        return jobBuilderFactory.get("Job")
                 .start(step1())
                 .next(step2())
+                .next(step3())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new CustomTasklet())
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(">> step1 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
                 .build();
     }
 
@@ -38,7 +45,18 @@ public class StepConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("step2 was executed");
+                    System.out.println(">> step2 was executed");
+//                    return RepeatStatus.FINISHED;
+                    throw new RuntimeException("step2 has failed");
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println(">> step3 was executed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
